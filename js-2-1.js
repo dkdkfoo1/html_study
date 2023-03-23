ba20d1da9faddf94c59539f7985661833f88eaa9
EunJi <125236683+dkdkfoo1@users.noreply.github.com>
2023-03-23 23:21:57 +0900

    Add files via upload

--- js-2-1.js
+++ js-2-1.js
@@ -8,12 +8,7 @@ console.log(result, result1, result2)
 
 answer_btn.addEventListener('click',function(){
     let result = document.querySelector('#result').value
-    //2.prompt 값 입력 (인증번호)
     result = Number(result)
-    // let num = window.prompt('입력하세요')
     result2.value = result
-    //3.2번 값의 데이터타입이 result_type출력
     result1.value = typeof result
-    //4.2번 값의 결과가 result 출력 
-    // console.log(this)
 })
